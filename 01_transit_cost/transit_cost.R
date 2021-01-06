e134267457f62b3230cb9626849b367632d58d88
Jonas Büchi <jo.buechi@gmail.com>
2021-01-06 22:04:24 +0100

    Comment test

--- 01_transit_cost/transit_cost.R
+++ 01_transit_cost/transit_cost.R
@@ -10,3 +10,4 @@ library(tidyverse)
 # load data --------------------------------------------------------------------
 transit_cost <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-05/transit_cost.csv')
 View(transit_cost)
+#Komentar
