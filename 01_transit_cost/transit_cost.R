5cb5d68ba806b9dca963b00486bcf7870d51e31c
Jonas Büchi <jo.buechi@gmail.com>
2021-01-06 21:57:32 +0100

    transit cost data set

---
+++ 01_transit_cost/transit_cost.R
@@ -0,0 +1,12 @@
+# info -------------------------------------------------------------------------
+# 
+# script by Jonas Sebastian Büchi
+# Jan 2021, Week 01
+# tidytuesday transit cost
+
+# load libraries ---------------------------------------------------------------
+library(tidyverse)
+
+# load data --------------------------------------------------------------------
+transit_cost <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-05/transit_cost.csv')
+View(transit_cost)
